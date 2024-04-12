efddafcbd4847f11eaf0ffcc4ee26ac55e05ee7f
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-12 09:48:18 -0600

    feat: get Order By Id

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -11,7 +12,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
   }
 
   create(createOrderDto: CreateOrderDto) {
-    
+
     return this.order.create({
       data:createOrderDto
     });
@@ -21,8 +22,17 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     return `This action returns all orders`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async findOne(id: string) {
+    const order = await this.order.findFirst({
+      where: { id }
+    });
+    if (!order) {
+      throw new RpcException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: `Order with id ${ id } not found`
+      })
+    }
+    return order;
   }
 
 
