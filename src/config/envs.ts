310c670adaf78de46b21e368638c67df5a731fde
Ricardo Blanco <149442545+RicardoBlancoRBT@users.noreply.github.com>
2024-04-15 14:50:24 -0600

    feat: return OrderDetail

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,12 +5,16 @@ import * as joi from 'joi';
 
 interface EnvVars {
     PORT: number;
-    DATABASE_URL: string;
+    // DATABASE_URL: string;
+    PRODUCTS_MICROSERVICE_HOST: string;
+    PRODUCTS_MICROSERVICE_PORT: number;
 }
 
 const envsSchema = joi.object({
     PORT: joi.number().required(),
     // DATABASE_URL: joi.string().required(),
+    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
 }).unknown();
 
 const {error, value} = envsSchema.validate(process.env);
@@ -21,5 +25,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
     port: envVars.PORT,
-    // databaseUrl: envVars.DATABASE_URL
+    // databaseUrl: envVars.DATABASE_URLç
+    PRODUCTS_MICROSERVICE_HOST: envVars.PRODUCTS_MICROSERVICE_HOST,
+    PRODUCTS_MICROSERVICE_PORT: envVars.PRODUCTS_MICROSERVICE_PORT,
 }
--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,23 +1,29 @@
-import { OrderStatus } from "@prisma/client";
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrderStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+import { OrderItemDto } from "./order-item.dto";
 
 export class CreateOrderDto {
 
-    @IsNumber()
-    @IsPositive()
-    totalAmount: number;
+    @IsArray()
+    @ArrayMinSize(1)
+    @ValidateNested({ each: true })
+    @Type(() => OrderItemDto )
+    items:OrderItemDto[]
+
+    // @IsNumber()
+    // @IsPositive()
+    // totalAmount: number;
     
-    @IsNumber()
-    @IsPositive()
-    totalItems: number;
+    // @IsNumber()
+    // @IsPositive()
+    // totalItems: number;
 
-    @IsEnum(OrderStatusList, { message: `Status must be one of these: ${OrderStatusList}` })
-    @IsOptional()
-    status: OrderStatus = OrderStatus.PENDING;
+    // @IsEnum(OrderStatusList, { message: `Status must be one of these: ${OrderStatusList}` })
+    // @IsOptional()
+    // status: OrderStatus = OrderStatus.PENDING;
 
-    @IsBoolean()
-    @IsOptional()
-    paid: boolean = false;
+    // @IsBoolean()
+    // @IsOptional()
+    // paid: boolean = false;
     
 }
